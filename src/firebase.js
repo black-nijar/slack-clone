ce427fa0137aa677375fb2f48333266e5f92e076
Nijarudeen <nijarr2020@gmail.com>
2021-02-28 18:56:11 +0530

    basic setup

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA_0yecrSj1--KrhGLPCDYk8xBpo-kNU5I",
+  authDomain: "slack-clone-b190b.firebaseapp.com",
+  projectId: "slack-clone-b190b",
+  storageBucket: "slack-clone-b190b.appspot.com",
+  messagingSenderId: "873388754528",
+  appId: "1:873388754528:web:9120999216f192ec42e505",
+  measurementId: "G-JEJPDSE4BV"
+};
